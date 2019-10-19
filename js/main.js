3f101d30a07b3eaa1881b7c0e6727af99c471fe3
Elena <nezh21@mail.ru>
2019-10-19 15:13:07 +0400

    Add libraries

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
 
-    //demo 01
-    $("#demo01").animatedModal();
-  
+    $("#main").animatedModal({
+        'color': '#55c57a',
+        'animatedIn': 'fadeInUp',
+        'animatedOut': 'fadeOutDown'
+    });
+
+    $("#section1").animatedModal({
+        'color': '#55c57a',
+        'animatedIn': 'fadeInUp',
+        'animatedOut': 'fadeOutDown'
+    });
+
+    $("#section2").animatedModal({
+        'color': '#55c57a',
+        'animatedIn': 'fadeInUp',
+        'animatedOut': 'fadeOutDown'
+    });
+
+    $("#section3").animatedModal({
+        'color': '#55c57a',
+        'animatedIn': 'fadeInUp',
+        'animatedOut': 'fadeOutDown'
+    });
+
+    $("#section4").animatedModal({
+        'color': '#55c57a',
+        'animatedIn': 'fadeInUp',
+        'animatedOut': 'fadeOutDown'
+    });
+
+    baguetteBox.run('.section-1__gallery, .section-2__gallery, .section-3__gallery, .section-4__gallery');
+
   });
