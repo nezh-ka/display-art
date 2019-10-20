a862cf4a8612408cb5cb09adfc9007a12f140822
Elena <nezh21@mail.ru>
2019-10-20 15:53:35 +0400

    Change a html-structure

--- js/main.js
+++ js/main.js
@@ -98,9 +98,46 @@ $(document).ready(function() {
     baguetteBox.run('.section-1__gallery, .section-2__gallery, .section-3__gallery, .section-4__gallery');
 
     // Смена языка
-    $('div[lang="en"], div[lang="tt"]').css('display', 'none');
 
-    $('button:contains("ru")').addClass('active');
+    $('div.eng, div.tat').css('display', 'none');
+
+    $('button:contains("ru")').click(function() {
+        $('div.ru').css('display', 'block');
+        $('div.eng, div.tat').css('display', 'none')
+    });
+
+    $('button:contains("tat")').click(function() {
+        $('div.tat').css('display', 'block');
+        $('div.eng, div.ru').css('display', 'none');
+    });
+
+    $('button:contains("eng")').click(function() {
+        $('div.eng').css('display', 'block');
+        $('div.tat, div.ru').css('display', 'none');
+    });
+
+
+/* $('div.eng, div.tat').css('opacity', '0');
+
+$('button:contains("ru")').click(function() {
+    $('div.ru').css('opacity', '1');
+    $('div.eng, div.tat').css('opacity', '0');
+});
+
+$('button:contains("tat")').click(function() {
+    $('div.tat').css('opacity', '1');
+    $('div.eng, div.ru').css('opacity', '0');
+});
+
+$('button:contains("eng")').click(function() {
+    $('div.eng').css('opacity', '1');
+    $('div.tat, div.ru').css('opacity', '0');
+}); */
+
+
+   /*  $('div[lang="en"], div[lang="tt"]').css('display', 'none');
+
+   // $('button:contains("ru")').addClass('active');
 
     $('button:contains("ru")').click(function() {
         // $(this).addClass('active');
@@ -122,14 +159,13 @@ $(document).ready(function() {
         $('div[lang="tt"]').css('display', 'block');
         $('div[lang="ru"], div[lang="en"]').css('display', 'none');
     });
-
+ */
     // Таймер возврата на главную страницу
-
-
-    if ($('.sections').css('opacity') == '1') {
-        setTimeout(function() {
-            $('.close-animatedSectionsRu').trigger('click');
-        }, 10000);
-    }
+    // console.log("opacity =", $('.sections').css('opacity'), +$('.sections').css('opacity'));
+    // if (+$('.sections').css('opacity') == 0) {
+    //     setTimeout(function() {
+    //         $('.close-animatedSectionsRu').trigger('click');
+    //     }, 5000);
+    // }
 
   });
