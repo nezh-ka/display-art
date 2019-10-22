66bd983a5a4c9c44db03e749f3c0167baa925643
Elena <nezh21@mail.ru>
2019-10-22 22:47:56 +0400

    Add an internationalization

--- js/main.js
+++ js/main.js
@@ -1,121 +1,37 @@
 $(document).ready(function() {
     // Анимация модальных окон
-    $("#main-ru").animatedModal({
+    $("#main").animatedModal({
         'color': '#55c57a',
         'animatedIn': 'fadeInUp',
         'animatedOut': 'fadeOutDown',
     });
 
-    $("#main-eng").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown',
-    });
-
-    $("#main-tat").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown',
-    });
-
-    $("#section1-ru").animatedModal({
+    $("#section1").animatedModal({
         'color': '#55c57a',
         'animatedIn': 'fadeInUp',
         'animatedOut': 'fadeOutDown'
     });
 
-    $("#section1-eng").animatedModal({
+    $("#section2").animatedModal({
         'color': '#55c57a',
         'animatedIn': 'fadeInUp',
         'animatedOut': 'fadeOutDown'
     });
 
-    $("#section1-tat").animatedModal({
+    $("#section3").animatedModal({
         'color': '#55c57a',
         'animatedIn': 'fadeInUp',
         'animatedOut': 'fadeOutDown'
     });
 
-    $("#section2-ru").animatedModal({
+    $("#section4").animatedModal({
         'color': '#55c57a',
         'animatedIn': 'fadeInUp',
         'animatedOut': 'fadeOutDown'
     });
-
-    
-    $("#section2-eng").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
-    
-    $("#section2-tat").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
-    $("#section3-ru").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
-    $("#section3-eng").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
-    $("#section3-tat").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
-    $("#section4-ru").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
-    $("#section4-eng").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
-    $("#section4-tat").animatedModal({
-        'color': '#55c57a',
-        'animatedIn': 'fadeInUp',
-        'animatedOut': 'fadeOutDown'
-    });
-
 
     // Галерея
     baguetteBox.run('.section-1__gallery, .section-2__gallery, .section-3__gallery, .section-4__gallery');
-
-    // Смена языка
-
-    $('div.eng, div.tat').css('display', 'none');
-
-    $('button:contains("ru")').click(function() {
-        $('div.ru').css('display', 'block');
-        $('div.eng, div.tat').css('display', 'none')
-    });
-
-    $('button:contains("tat")').click(function() {
-        $('div.tat').css('display', 'block');
-        $('div.eng, div.ru').css('display', 'none');
-    });
-
-    $('button:contains("eng")').click(function() {
-        $('div.eng').css('display', 'block');
-        $('div.tat, div.ru').css('display', 'none');
-    });
-
-
 
     // Таймер возврата на главную страницу
     console.log("opacity =", $('.sections').css('opacity'), +$('.sections').css('opacity'));
@@ -158,37 +74,5 @@ $(document).ready(function() {
 //         $('button:contains("eng")').addClass('active');
 //         $('button:contains("ru"), button:contains("tat")').removeClass('active');
 //     }
-   /*  $('div.eng, div.tat').css('display', 'none');
-    // Смена языков на главной странице
-    $('.main__langs button:contains("ru")').click(function() {
-        $('div.main.ru').css('display', 'block');
-        $('div.main.tat, div.main.eng').css('display', 'none');
-    });
-
-    $('.main__langs button:contains("tat")').click(function() {
-        $('div.main.tat').css('display', 'block');
-        $('div.main.ru, div.main.eng').css('display', 'none');
-    });
-
-    $('.main__langs button:contains("eng")').click(function() {
-        $('div.main.eng').css('display', 'block');
-        $('div.main.ru, div.main.tat').css('display', 'none');
-    });
-
-    // Смена языков на странице выбора
-    $('.sections button:contains("ru")').click(function() {
-        $('.sections ru').css('display', 'block');
-        $('.sections tat, .sections eng').css('display', 'none');
-    });
-
-    $('.sections button:contains("tat")').click(function() {
-        $('.sections tat').css('display', 'block');
-        $('.sections ru, .sections eng').css('display', 'none');
-    });
-
-    $('.sections button:contains("eng")').click(function() {
-        $('.sections eng').css('display', 'block');
-        $('.sections ru, .sections tat').css('display', 'none');
-    }); */
 
   });
